4788b1ac67d9eccb05050e32d376e9325582794b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-17 01:06:12 +0200

    Add stub

---
+++ Leader.pm
@@ -0,0 +1,26 @@
+package MARC::Leader;
+
+use strict;
+use warnings;
+
+use Class::Utils qw(set_params);
+use Error::Pure qw(err);
+
+our $VERSION = 0.01;
+
+# Constructor.
+sub new {
+	my ($class, @params) = @_;
+
+	# Create object.
+	my $self = bless {}, $class;
+
+	# Process parameters.
+	set_params($self, @params);
+
+	return $self;
+}
+
+1;
+
+__END__
