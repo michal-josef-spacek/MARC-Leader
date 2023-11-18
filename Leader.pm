c90c96e702965d4d6d8bb223433564e503b29bf0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-18 18:56:26 +0100

    Add serialize() example to doc

--- Leader.pm
+++ Leader.pm
@@ -143,7 +143,7 @@ Returns string.
          Bad 'Data::MARC::Leader' instance to serialize.
 
 
-=head1 EXAMPLE
+=head1 EXAMPLE1
 
 =for comment filename=parse_marc_leader_and_dump.pl
 
@@ -197,6 +197,48 @@ Returns string.
  #         undefined                         0
  #     }
  # }
+
+=head1 EXAMPLE2
+
+=for comment filename=serialize_marc_leader.pl
+
+ use strict;
+ use warnings;
+
+ use Data::MARC::Leader;
+ use MARC::Leader;
+
+ # Object.
+ my $obj = MARC::Leader->new;
+
+ # Data object.
+ my $data_marc_leader = Data::MARC::Leader->new(
+         'bibliographic_level' => 'm',
+         'char_encoding_scheme' => 'a',
+         'data_base_addr' => 541,
+         'descriptive_cataloging_form' => 'i',
+         'encoding_level' => ' ',
+         'impl_def_portion_len' => '0',
+         'indicator_count' => '2',
+         'length' => 2200,
+         'length_of_field_portion_len' => '4',
+         'multipart_resource_record_level' => ' ',
+         'starting_char_pos_portion_len' => '5',
+         'status' => 'c',
+         'subfield_code_count' => '2',
+         'type' => 'e',
+         'type_of_control' => ' ',
+         'undefined' => '0',
+ );
+
+ # Serialize.
+ my $leader = $obj->serialize($data_marc_leader);
+
+ # Print to output.
+ print $leader."\n";
+
+ # Output:
+ # 02200cem a2200541 i 4500
 
 =head1 DEPENDENCIES
 
