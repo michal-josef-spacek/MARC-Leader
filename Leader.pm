ff520625ebba91dd3edcfb672fbb9f2fb21945b2
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2026-04-28 13:25:04 +0200

    Prepare for 0.10 version

--- Leader.pm
+++ Leader.pm
@@ -10,7 +10,7 @@ use Error::Pure qw(err);
 use Mo::utils 0.06 qw(check_bool);
 use Scalar::Util qw(blessed);
 
-our $VERSION = 0.09;
+our $VERSION = 0.10;
 
 # Constructor.
 sub new {
@@ -407,6 +407,6 @@ the Czech Republic (DKRVO 2024–2028), Area 11: Linked Open Data.
 
 =head1 VERSION
 
-0.09
+0.10
 
 =cut
